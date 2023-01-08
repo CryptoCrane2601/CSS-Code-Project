5fa2e9fa905aa42166a57e1473b15345652930ca
CryptoCrane2601 <dejan.kurcubic@icloud.com>
2023-01-08 12:16:56 +0100

    add function for No button

--- shared.js
+++ shared.js
@@ -1,6 +1,7 @@
 var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
 var selectPlanButtons = document.querySelectorAll('.plan button');
+var selectNoButton = document.querySelectorAll('.modal__action--negative');
 
 
 // console.dir(selectPlanButtons);
@@ -12,5 +13,10 @@ for (var i = 0; i < selectPlanButtons.length; i++) {
         });
 }
 
-
+for (var i =0; i < selectNoButton.length; i++) {
+    selectNoButton[i].addEventListener('click', function() {
+        modal.style.display = 'none';
+        backdrop.style.display = 'none';
+    });
+}
 
