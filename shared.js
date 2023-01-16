6c6676bc338121548696bc17b707a941cb86123c
CryptoCrane2601 <dejan.kurcubic@icloud.com>
2023-01-17 00:09:42 +0100

    changed java file

--- shared.js
+++ shared.js
@@ -1,47 +1,54 @@
-var backdrop = document.querySelector('.backdrop');
-var modal = document.querySelector('.modal');
-var selectNoButton = document.querySelector('.modal__action--negative');
-var toggleButton = document.querySelector('.toggle-button');
-var mobileNav = document.querySelector('.mobile-nav');
-var selectPlanButtons = document.querySelectorAll('.plan button');
+var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
+var modalNoButton = document.querySelector(".modal__action--negative");
+var selectPlanButtons = document.querySelectorAll(".plan button");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
 
+// console.dir(backdrop.style['background-image']);
 
-
-// console.dir(selectPlanButtons);
-
+// console.dir(backdrop);
 for (var i = 0; i < selectPlanButtons.length; i++) {
-    selectPlanButtons[i].addEventListener('click', function() {
-        // modal.style.display = 'block';
-        // backdrop.style.display = 'block';
-        modal.classList.add('open')
-        backdrop.classList.add('open')
-        });
+  selectPlanButtons[i].addEventListener("click", function() {
+    // modal.style.display = "block";
+    // backdrop.style.display = "block";
+    // modal.className = 'open'; // This will actually overwrite the complete class list
+    modal.classList.add("open");
+    backdrop.style.display = "block";
+    setTimeout(function() {
+      backdrop.classList.add("open");
+    }, 10);
+  });
 }
-backdrop.addEventListener('click', function() {
-    // mobileNav.style.display = 'none';
-    // backdrop.style.display = 'none';    
-    // modal.style.display = 'none';  
-    mobileNav.classList.remove('open');
-    backdrop.classList.remove('open');
-    if (modal) {
-        modal.classList.remove('open');
-    }
-    
-    });
 
-if(selectNoButton) {
-    selectNoButton.addEventListener('click', function() {
-    // backdrop.style.display = 'none';    
-    // modal.style.display = 'none';
-    backdrop.classList.remove('open'); 
-    modal.classList.remove('open'); 
-    });
+backdrop.addEventListener("click", function() {
+  // mobileNav.style.display = 'none';
+  mobileNav.classList.remove("open");
+  closeModal();
+});
+
+if (modalNoButton) {
+  modalNoButton.addEventListener("click", closeModal);
 }
-    
 
-toggleButton.addEventListener('click', function() {
-    // mobileNav.style.display = 'block';
-    // backdrop.style.display = 'block';
-    mobileNav.classList.add('open');
-    backdrop.classList.add('open');
-})
+function closeModal() {
+  //   backdrop.style.display = "none";
+  //   modal.style.display = "none";
+  if (modal) {
+    modal.classList.remove("open");
+  }
+  backdrop.classList.remove("open");
+  setTimeout(function() {
+    backdrop.style.display = "none";
+  }, 200);
+}
+
+toggleButton.addEventListener("click", function() {
+  // mobileNav.style.display = 'block';
+  // backdrop.style.display = 'block';
+  mobileNav.classList.add("open");
+  backdrop.style.display = "block";
+  setTimeout(function() {
+    backdrop.classList.add("open");
+  }, 10);
+});
